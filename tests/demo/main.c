12c45ed8927fa7b43ff26d4e2b8d155b08bb0cff
Abert Chen <albert.chen@sifive.com>
2019-10-18 11:25:38 -0700

    add pio with different width and base addr

--- tests/demo/main.c
+++ tests/demo/main.c
@@ -4,11 +4,18 @@
 
 int main() {
     // read/write to axi block
+#ifdef PIO_WIDTH_16
+    uint16_t odatas[5] = {0xBEEF, 0xF0F0, 0x1234, 0x4567, 0xBA98};
+    uint16_t oenables[5] = {0xF0F0, 0x0F0F, 0xBEEF, 0xCDEF, 0x321};
+    uint16_t read_value;
+#else
     uint32_t odatas[5] = {0xDEADBEEF, 0xF0F0F0F0, 0xABCD1234, 0x01234567,
                           0xFEDCBA98};
     uint32_t oenables[5] = {0xF0F0F0F0, 0x0F0F0F0F, 0xDEADBEEF, 0x89ABCDEF,
                             0x7654321};
     uint32_t read_value;
+#endif
+
     const struct metal_pio *m_pio = get_metal_pio(0);
     int fail = 0;
 
